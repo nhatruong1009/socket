496ab45c3edb6040466bab58c095126581898a68
Samuel <20127610@student.hcmus.edu.vn>
2021-07-21 21:36:38 +0700

    dsa

--- socket/winclient/Source.cpp
+++ socket/winclient/Source.cpp
@@ -7,10 +7,77 @@
 
 #pragma comment (lib, "Ws2_32.lib")
 
-void severConnect(int sever) 
+void serverConnect(SOCKET& Socket, char* argv)
+{
+	int result;
+	//-------------------------
+	// Khởi tạo WSA (cần thiết để khởi tạo socket)
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) < 0)
+	{
+		std::cout << "WSA initialize failed" << std::endl;
+		return;
+	}
+	else
+	{
+		std::cout << "WSA initialize successfully" << std::endl;
+	}
+
+	//-------------------------
+	addrinfo hints, * addr = nullptr;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	// Resolve the server address and port
+	result = getaddrinfo(argv, PORT, &hints, &addr);
+	if (result != 0)
+	{
+		printf("getaddrinfo failed with error: %d\n", result);
+		WSACleanup();
+		return;
+	}
+
+	//-------------------------
+	// Initialize Socket
+	// Attempt to connect to an address until one succeeds
+	for (addrinfo* ptr = addr; ptr != nullptr; ptr = ptr->ai_next) {
+
+		// Create a SOCKET for connecting to server
+		Socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (Socket == INVALID_SOCKET)
+		{
+			std::cout << "socket failed with error: " << WSAGetLastError() << std::endl;
+			WSACleanup();
+			return;
+		}
+
+		// Connect to server.
+		result = connect(Socket, ptr->ai_addr, (int)ptr->ai_addrlen);
+		if (result == SOCKET_ERROR)
+		{
+			closesocket(Socket);
+			Socket = INVALID_SOCKET;
+		}
+		else
+			break;
+	}
+	freeaddrinfo(addr);
+	if (Socket == INVALID_SOCKET)
+	{
+		std::cout << "Unable to connect to server!\n";
+		WSACleanup();
+		return;
+	}
+
+	//WSACleanup();	// 10093
+	//closesocket(Socket);	// 10038
+}
+
+void Get(SOCKET Socket, char* user)
 {
 
-	closesocket(sever);
 }
 
 void Login(SOCKET Socket)
@@ -28,16 +95,13 @@ void Login(SOCKET Socket)
 	std::cout << user << "\n" << pass <<"\n";
 	char KEY[2];
 	int result;
-	do
-	{
-		result = send(Socket, "IN", 2, 0);
-	}
-	while (result < 0);
+	result = send(Socket, "IN", 3, 0);
 	send(Socket, user, n1, 0);
 	send(Socket, user, n2, 0);
 	result = recv(Socket, KEY, 2, 0);
-	if (result >= 0)
+	if (result >= 0 && KEY == "1")
 		std::cout << "Login successfully\n";
+	else std::cout << "Login failed\n";
 
 }
 
@@ -60,12 +124,12 @@ void Register(SOCKET Socket)
 		std::cin >> temp1;
 		std::cout << "Password: ";
 		std::cin >> temp2;
-		std::cout << "Confirm password: ";
+		std::cout << "Confirm paswword: ";
 		std::cin >> temp3;
 		if (temp2 != temp3)
 		{
 			system("cls");
-			std::cout << "Confirm password does not match\n";
+			std::cout << "Confirm password does not match your password\n";
 		}
 	} while (temp2 != temp3);
 	int n1 = temp1.size() + 1, n2 = temp2.size() + 1;
@@ -76,80 +140,17 @@ void Register(SOCKET Socket)
 	std::cout << user << "\n" << pass << "\n";
 	char KEY[2];
 	int result;
-	do
+	result = send(Socket, "REG", 4, 0);
+	if (result < 0)
 	{
-		result = send(Socket, "IN", 2, 0);
-	} 	while (result < 0);
+		std :: cout << "send failed with error:" << WSAGetLastError() << std::endl;
+	}
 	send(Socket, user, n1, 0);
 	send(Socket, user, n2, 0);
 	result = recv(Socket, KEY, 2, 0);
-	if (result >= 0)
+	if (result >= 0 && KEY == "1")
 		std::cout << "Register successfully\n";
-}
-
-void getStarted(SOCKET& Socket, char* argv)
-{
-	int result;
-	//-------------------------
-	// Khởi tạo WSA (cần thiết để khởi tạo socket)
-	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) < 0)
-	{
-		std::cout << "WSA initialize failed" << std::endl;
-		return ;
-	}
-	else
-	{
-		std::cout << "WSA initialize successfully" << std::endl;
-	}
-
-	//-------------------------
-	addrinfo hints, * addr = nullptr;
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-
-	// Resolve the server address and port
-	result = getaddrinfo(argv, PORT, &hints, &addr);
-	if (result != 0)
-	{
-		printf("getaddrinfo failed with error: %d\n", result);
-		WSACleanup();
-		return ;
-	}
-
-	//-------------------------
-	// Initialize Socket
-	// Attempt to connect to an address until one succeeds
-	for (addrinfo* ptr = addr; ptr != nullptr; ptr = ptr->ai_next) {
-
-		// Create a SOCKET for connecting to server
-		Socket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-		if (Socket == INVALID_SOCKET)
-		{
-			std::cout << "socket failed with error: " << WSAGetLastError() << std::endl;
-			WSACleanup();
-			return ;
-		}
-
-		// Connect to server.
-		result = connect(Socket, ptr->ai_addr, (int)ptr->ai_addrlen);
-		if (result == SOCKET_ERROR)
-		{
-			closesocket(Socket);
-			Socket = INVALID_SOCKET;
-		}
-		else
-			break;
-	}
-	freeaddrinfo(addr);
-	if (Socket == INVALID_SOCKET)
-	{
-		std::cout << "Unable to connect to server!\n";
-		WSACleanup();
-		return ;
-	}
+	else std::cout << "Register failed\n";
 }
 
 int main(int argc, char** argv) 
@@ -162,7 +163,10 @@ int main(int argc, char** argv)
 	SOCKET Socket = 1;
 	//-------------------------
 	/*getStarted(Socket, argv[1]);*/
-	Login(Socket);
+	serverConnect(Socket, argv[1]);
+	Register(Socket);
+	WSACleanup();
+	closesocket(Socket);
 	return 0;
 }
 
